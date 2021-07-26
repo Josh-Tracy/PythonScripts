df2dfeac2466982572aad9744fc4be7f0e7da1bc
JOSHUA <darkjosht@hotmail.com>
2021-07-26 18:35:06 -0400

    Excel auatomation added

---
+++ Excel Automation/main.py
@@ -0,0 +1,18 @@
+import openpyxl
+
+# Load the workbook from the project directory and its contents
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+
+    if supplier_name in products_per_supplier:
+        products_per_supplier[supplier_name] = products_per_supplier[supplier_name] + 1
+    else:
+        print("Adding a new supplier")
+        products_per_supplier[supplier_name] = 1
+
+print(products_per_supplier)
